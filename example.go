97b1e43d51ac44d9e70442fdefcd142799e324cc
Matthieu Thienpont <mthienpont@Matthieus-MacBook-Pro.local>
2021-01-12 16:57:37 +0000

    Added async handler example

--- example.go
+++ example.go
@@ -4,14 +4,15 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/dgrr/fastws"
 	"github.com/gofiber/fiber/v2"
 )
 
 func wsHandler(conn *Conn) {
 	fmt.Fprintf(conn, "Hello world")
-	var b []byte
+
 	for {
-		mt, msg, err := conn.ReadMessage(b)
+		mt, msg, err := conn.ReadMessage(nil)
 		if err != nil {
 			log.Println("read:", err)
 			break
@@ -25,9 +26,38 @@ func wsHandler(conn *Conn) {
 	}
 }
 
+func asyncHandler(conn *Conn) {
+	dataChannel := make(chan []byte)
+	okChannel := make(chan bool)
+
+	go listenToIncoming(dataChannel, conn)
+	go echoReply(dataChannel, conn)
+
+	working := <-okChannel
+	log.Println(working)
+}
+
+func listenToIncoming(dataChannel chan []byte, conn *Conn) {
+	for {
+		_, msg, err := conn.ReadMessage(nil)
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", msg)
+		dataChannel <- msg
+	}
+}
+
+func echoReply(dataChannel chan []byte, conn *Conn) {
+	for data := range dataChannel {
+		conn.WriteMessage(fastws.ModeBinary, data)
+	}
+}
+
 func main() {
 	app := fiber.New()
-	app.Get("/ws", New(wsHandler))
+	app.Get("/ws", New(asyncHandler))
 
 	app.Listen(":3000") // ws://localhost:3000/ws
 }
